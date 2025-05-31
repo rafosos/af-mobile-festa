7a76a751a1dec688cee8b4525697a123f2649124
rafosos <rafael.lrfrancisco@gmail.com>
2025-05-30 21:59:43 -0300

    criação do projeto

---
+++ app/src/main/java/com/example/afmobilefesta/Card.java
@@ -0,0 +1,36 @@
+package com.example.afmobilefesta;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Card extends Fragment {
+
+    private CardViewModel mViewModel;
+
+    public static Card newInstance() {
+        return new Card();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_card, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(CardViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/afmobilefesta/CardViewModel.java
@@ -0,0 +1,7 @@
+package com.example.afmobilefesta;
+
+import androidx.lifecycle.ViewModel;
+
+public class CardViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
